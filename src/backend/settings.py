c0c771aad84c8af95da0953f180d59c6ba3ef527
NatLee <natlee.work@gmail.com>
2023-11-02 16:33:09 +0800

    [update] add new settings

--- src/backend/settings.py
+++ src/backend/settings.py
@@ -9,10 +9,18 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import sys
+from datetime import timedelta
+import logging
 from pathlib import Path
+
+logger = logging.getLogger(__name__)
+
+print("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+print(f"---------- Project DIR: {BASE_DIR}")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -20,13 +28,38 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-uye!=@*gjois#e#8u*3law8=d4^&5s0sh-w*+r3_%+x5tn$lru'
 
+
+# ----------------------------- START - DEBUG setting -------------------------------
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+print(f"---------- Debug mode: {DEBUG}")
+# ------------------------------ END - DEBUG setting --------------------------------
 
 ALLOWED_HOSTS = ['*']
 
-# Application definition
+# -------------- START - CORS Setting --------------
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CSRF_TRUSTED_ORIGINS = [
+    "http://127.0.0.1",
+    "http://localhost",
+]
+# -------------- END - CORS Setting -----------------
 
+
+# -------------- START - Google Auth Setting --------------
+SECURE_REFERRER_POLICY = "no-referrer-when-downgrade"
+# SECURE_CROSS_ORIGIN_OPENER_POLICY = "same-origin-allow-popups"
+SECURE_CROSS_ORIGIN_OPENER_POLICY = None
+SOCIAL_GOOGLE_CLIENT_ID = (
+    "376808175534-d6mefo6b1kqih3grjjose2euree2g3cs.apps.googleusercontent.com"
+)
+LOGIN_REDIRECT_URL = "/"
+VALID_REGISTER_DOMAINS = ["gmail.com"]
+# --------------- END - Google Auth Setting -----------------
+
+
+# Application definition
 INSTALLED_APPS = [
     # django
     'django.contrib.admin',
@@ -35,9 +68,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # base
+    "rest_framework",
+    "drf_yasg",
     # package
     'django_rq',
     # apps
+    "custom_jwt",
+    "custom_auth",
     'xterm',
 ]
 
@@ -75,16 +113,37 @@ TEMPLATES = [
 ASGI_APPLICATION = "backend.asgi.application"
 WSGI_APPLICATION = "backend.wsgi.application"
 
+# -------------- START - Swagger Setting --------------
 
+USE_X_FORWARDED_HOST = True
+
+SWAGGER_SETTINGS = {
+    "SECURITY_DEFINITIONS": {
+        "Token(add prefix `Bearer` yourself)": {
+            "type": "apiKey",
+            "name": "Authorization",
+            "in": "header",
+        }
+    },
+    "LOGIN_URL": "/api/__hidden_admin/login",
+    "LOGOUT_URL": "/api/__hidden_admin/logout/?next=/api/__hidden_swagger",
+}
+
+# --------------- END - Swagger Setting----------------
+
+
+# -------------- START - Database Setting --------------
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+SQLITE_DIR = BASE_DIR / "db.sqlite3"
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": SQLITE_DIR,
     }
 }
+print(f"---------- SQLITE DIR: {SQLITE_DIR}")
+# -------------- END - Database Setting --------------
 
 
 # Password validation
@@ -109,20 +168,19 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
+LANGUAGE_CODE = "en-us"
+TIME_ZONE = "Asia/Taipei"
 USE_I18N = True
-
+USE_L10N = True
 USE_TZ = True
+APPEND_SLASH = False
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-
+STATIC_URL = "api/__hidden_statics/"
+STATIC_ROOT = "staticfiles"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
     # other folders containing static files
@@ -147,3 +205,121 @@ CACHES = {
         'LOCATION': 'redis://d-docker-redis:6379',
     },
 }
+
+# Default primary key field type
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# -------------- START - Log setting --------------
+LOG_ROOT = Path(BASE_DIR) / 'logs'
+LOG_ROOT.mkdir(exist_ok=True)
+
+LOG_TYPES = ['file', 'database']
+for log_type in LOG_TYPES:
+    log_type_path = Path(BASE_DIR) / 'logs' / log_type
+    log_type_path.mkdir(exist_ok=True)
+
+HANDLERS = {}
+
+for log_type in LOG_TYPES:
+    HANDLERS[log_type] = {
+        'class': 'common.log.InterceptTimedRotatingFileHandler',
+        'filename': f"{LOG_ROOT / log_type/ f'{log_type}.log'}",
+        'when': "H",
+        'interval': 1,
+        'backupCount': 1,
+        'formatter': 'standard',
+        'encoding': 'utf-8',
+    }
+
+HANDLERS.update({
+    'console': {
+        'class': 'logging.StreamHandler',
+        'stream': sys.stdout
+    }
+})
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        # LOG格式
+        'standard': {
+            'format': '[%(asctime)s] [%(filename)s:%(lineno)d] [%(module)s:%(funcName)s] [%(levelname)s] - %(message)s'},
+        'simple': {  # 簡單格式
+            'format': '%(levelname)s %(message)s'
+        }
+    },
+    'filters': {
+    },
+    'handlers': HANDLERS,
+    'loggers': {
+        'django': {
+            'handlers': ['file', 'console'],
+            'propagate': True,
+            'level': "INFO"
+        },
+        'django.db.backends': {
+            'handlers': ['database'],
+            'propagate': False,
+            'level': "DEBUG"
+        },
+        'django.request': {
+            'handlers': ['file'],
+            'propagate': False,
+            'level': "DEBUG"
+        }
+    }
+}
+
+# --------------- END - Log setting ---------------
+
+
+# ---------------------------- START - REST_FRAMEWORK setting --------------------------
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        # 支援使用simplejwt的JWT登入
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+        # 支援一般session進行後台登入
+        "rest_framework.authentication.SessionAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": (
+        # restframework的API必須登入才可使用
+        "rest_framework.permissions.IsAuthenticated",
+    ),
+    # 使用JSON來render API而非HTML界面
+    "DEFAULT_RENDERER_CLASSES": ("rest_framework.renderers.JSONRenderer",),
+}
+# ----------------------------- END - REST_FRAMEWORK setting ----------------------------
+
+
+# -------------- Start - SimpleJWT Setting --------------
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=3600),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ROTATE_REFRESH_TOKENS": False,
+    "BLACKLIST_AFTER_ROTATION": False,
+    "UPDATE_LAST_LOGIN": False,
+    "ALGORITHM": "HS256",
+    "SIGNING_KEY": SECRET_KEY,
+    "VERIFYING_KEY": None,
+    "AUDIENCE": None,
+    "ISSUER": None,
+    "JWK_URL": None,
+    "LEEWAY": 0,
+    "AUTH_HEADER_TYPES": ("Bearer",),
+    "AUTH_HEADER_NAME": "HTTP_AUTHORIZATION",
+    "USER_ID_FIELD": "id",
+    "USER_ID_CLAIM": "user_id",
+    "USER_AUTHENTICATION_RULE": "rest_framework_simplejwt.authentication.default_user_authentication_rule",
+    "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken",),
+    "TOKEN_TYPE_CLAIM": "token_type",
+    "TOKEN_USER_CLASS": "rest_framework_simplejwt.models.TokenUser",
+    "JTI_CLAIM": "jti",
+    "SLIDING_TOKEN_REFRESH_EXP_CLAIM": "refresh_exp",
+    "SLIDING_TOKEN_LIFETIME": timedelta(minutes=5),
+    "SLIDING_TOKEN_REFRESH_LIFETIME": timedelta(days=1),
+}
+# -------------- END - SimpleJWT Setting --------------
+
+
+print("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-")
