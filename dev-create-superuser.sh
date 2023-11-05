d51b81b3a986069030248dc62500b5c9bac0a4c9
NatLee <natlee.work@gmail.com>
2023-11-06 00:12:28 +0800

    [fix] name of container

--- dev-create-superuser.sh
+++ dev-create-superuser.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker exec -it d-docker-ui bash -c 'DJANGO_SUPERUSER_PASSWORD=1234 python manage.py createsuperuser --noinput --username admin --email admin@admin.com'
+docker exec -it d-docker-web bash -c 'DJANGO_SUPERUSER_PASSWORD=1234 python manage.py createsuperuser --noinput --username admin --email admin@admin.com'
