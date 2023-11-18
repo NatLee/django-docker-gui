ffbee4b9045019c895702603cf76e6f73593872c
NatLee <natlee.work@gmail.com>
2023-11-18 21:38:08 +0800

    [update] add `notification` ws

--- src/xterm/consumers.py
+++ src/xterm/consumers.py
@@ -6,7 +6,9 @@ import select
 import docker
 
 from asgiref.sync import sync_to_async
+from asgiref.sync import async_to_sync
 
+from channels.layers import get_channel_layer
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 class ConsoleConsumer(AsyncWebsocketConsumer):
@@ -140,7 +142,6 @@ class ConsoleConsumer(AsyncWebsocketConsumer):
         self.scope["session"].update(session_data)
         await sync_to_async(self.scope["session"].save)()
 
-
 class PullConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.docker_socket = None
@@ -196,4 +197,44 @@ class PullConsumer(AsyncWebsocketConsumer):
 
         await asyncio.sleep(1.5)
         await self.close()
+
+class NotificationConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_name = 'notifications'  # Can be dynamic based on path
+        self.room_group_name = f'group_{self.room_name}'
+
+        # Join room group
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        # Leave room group
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+
+    # Receive message from room group
+    async def send_notification(self, event):
+        message = event['message']
+
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps({
+            'message': message
+        }))
+
+
+def send_notification_to_group(message):
+    channel_layer = get_channel_layer()
+    async_to_sync(channel_layer.group_send)(
+        'group_notifications',  # Must match the group name used in your consumer
+        {
+            'type': 'send_notification',
+            'message': message
+        }
+    )
 
